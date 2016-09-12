2e1f833af43dae883f386368ef8600f9d92350e1
lixiang34 <lixiang34@baidu.com>
2016-09-12 19:53:41 +0800

    费率设置

--- MakeMoney/Judge/ExchangeSetting.cs
+++ MakeMoney/Judge/ExchangeSetting.cs
@@ -9,8 +9,12 @@ namespace Exchange
     // 购买费率的配置类
     class ExchangeSetting
     {
-        public decimal buyRate;
+        public decimal yinHua = 0.001M;//印花只卖出收
 
-        public decimal saleRate;
+        public decimal yongJin = 0.0003M;//佣金双向
+
+        public decimal minYongJin = 5;//佣金最低5元
+
+        public decimal guoHu = 1;//过户每1000股1元
     }
 }
